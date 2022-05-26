c32e7e7d32733a4bbde9abff0016f0f4a6d0e4f2
player666 <2040691859@qq.com>
2022-05-26 13:23:09 +0800

    second commit

--- src/main/java/com/example/gitdemo/com/hqh/Test.java
+++ src/main/java/com/example/gitdemo/com/hqh/Test.java
@@ -6,5 +6,9 @@ package com.example.gitdemo.com.hqh;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
