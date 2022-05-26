7314631b554293e1128c5513e7ab1b2c0489eb11
player666 <2040691859@qq.com>
2022-05-26 11:54:50 +0800

    first commit

---
+++ src/main/java/com/example/gitdemo/com/hqh/Test.java
@@ -0,0 +1,10 @@
+package com.example.gitdemo.com.hqh;
+
+/**
+ * @ Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
